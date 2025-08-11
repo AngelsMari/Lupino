1a9157f9fbf45b769a639f884e75b9241b1de080
AngelsMari <67146372+AngelsMari@users.noreply.github.com>
2025-08-11 18:50:16 +0200

    Page d'accueil

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { UserService } from 'app/services/LupinoApi/user.service';
 import { Subject } from 'rxjs';
 import { map, takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css'],
-    standalone: false
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css'],
+	standalone: false,
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+	router = inject(Router);
 	private destroy$ = new Subject<void>();
 	isLoggedIn$ = this.authService.loggedIn$; // Observable<boolean>
 	isAdmin$ = this.userService.getUserData().pipe(map((user) => user?.isAdmin ?? false));
 
-	constructor(private authService: AuthService, private userService: UserService) {}
+	constructor(
+		private authService: AuthService,
+		private userService: UserService,
+	) {}
 
 	ngOnInit() {}
 
